7452132af20d7a6468391a0feb665f0afb045207
Michele <michele.filosi@gmail.com>
2025-02-12 09:39:55 +0100

    Solved bug in genetic file separation

--- workflow/scripts/divide_by_chrom.py
+++ workflow/scripts/divide_by_chrom.py
@@ -8,9 +8,12 @@ def main(genotype_conf, chrom, ofile):
     """This function will separate the genetic data into chromosomes
     """
     if genotype_conf["divided_by_chrom"]:
-        shutil.copy(genotype_conf["files"][chrom], ofile)
+        for k, v in ofile.items():
+            infile = genotype_conf["files"][chrom] + f".{k}"
+            shutil.copy(infile, v)
     else:
         print(genotype_conf["files"]["1"])
+        ofile = ofile["bed"].replace(".bed", "")
         sb.run(["plink",  "--bfile",
                 genotype_conf["files"]["1"],
                 "--chr", str(chrom),
@@ -21,7 +24,8 @@ def main(genotype_conf, chrom, ofile):
 
 if __name__ == "__main__":
     genotype_conf = snakemake.params.genotype_conf
-    ofile = snakemake.output.bed
+    print(snakemake.output)
+    ofile = snakemake.output
     chrom = snakemake.wildcards.chrom
-    ofile = ofile.replace(".bed", "")
+    # ofile = ofile.replace(".bed", "")
     main(genotype_conf=genotype_conf, chrom=chrom, ofile=ofile)
